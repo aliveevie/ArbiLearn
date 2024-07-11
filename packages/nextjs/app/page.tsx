75a097dd70921e41f4fe598674565cb781582f22
Ibrahim <aliveibi080@gmail.com>
2024-07-11 16:58:46 +0100

    Update files

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,14 +2,14 @@
 
 // import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
+// import { useAccount } from "wagmi";
 // import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 // import { Address } from "~~/components/scaffold-eth";
 import HomePage from "~~/mycomponents/home";
 
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+  // const { address: connectedAddress } = useAccount();
 
   return (
     <>
