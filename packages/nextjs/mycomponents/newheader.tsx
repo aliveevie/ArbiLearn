3a2e993fd450575bc5ac0995ef6090f35dfcd5d9
Ibrahim <aliveibi080@gmail.com>
2024-07-11 11:08:49 +0100

    Update files

---
+++ packages/nextjs/mycomponents/newheader.tsx
@@ -0,0 +1,8 @@
+import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+
+export const Header = () => {
+    return <>
+        <RainbowKitCustomConnectButton/>
+        <FaucetButton />
+    </>
+}
