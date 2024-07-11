7414d49fb1f3e62cc74c9032f551706d5389171e
Ibrahim <aliveibi080@gmail.com>
2024-07-11 16:38:18 +0100

    Update files

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Link from "next/link";
+// import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+// import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+// import { Address } from "~~/components/scaffold-eth";
 import HomePage from "~~/mycomponents/home";
 
 
