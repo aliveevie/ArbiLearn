2989888d8439001d59f2950c2bf634c68fec3e67
Ibrahim <aliveibi080@gmail.com>
2024-07-22 06:31:30 +0100

    Update files

--- src/mycomponents/newheader.tsx
+++ src/mycomponents/newheader.tsx
@@ -1,15 +1,14 @@
-import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import Image from 'next/image';
-import logo from '../public/logo.png'; // Make sure to replace 'public/logo.png' with the path to your logo image
+import logo from '../../public/logo.png'; // Make sure to replace 'public/logo.png' with the path to your logo image
 
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 bg-gray-100">
+    <header className="flex justify-between items-center p-4 bg-gray-100 w-full">
       <div className="flex items-center">
         <Image src={logo} alt="Logo" width={100} height={100} />
       </div>
-      <div className="flex items-center space-x-4">
-        <RainbowKitCustomConnectButton />
+      <div className="flex items-center ml-auto">
+        <w3m-button />
       </div>
     </header>
   );
