9a719e99b122ad6b48fbca75af4f803d4cd08de7
Ibrahim Abdulkarim <macbookair@MacBooks-MacBook-Air.local>
2024-10-07 12:16:05 +0100

    Update files

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 import { useWeb3Modal } from '@web3modal/wagmi/react'
 import { HeaderComponent } from "./header";
 import { CoursesCatalogComponent } from "./components-courses-catalog-component";
-
+import { ProgramsCatalogComponent } from "./components-programs-catalog-component";
 
 
 export function Hero() {
@@ -140,6 +140,7 @@ export function Hero() {
         </section>
         <section>
           <CoursesCatalogComponent />
+          <ProgramsCatalogComponent />
         </section>
         <section id="testimonials" className="w-full py-12 md:py-24 lg:py-32">
           <div className="container mx-auto grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
