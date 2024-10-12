0f5b09bf650392706f4cf4d2278294eb03c0d677
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-10-12 11:23:24 +0100

    Update files

--- src/app/apis/test/route.tsx
+++ src/app/apis/test/route.tsx
@@ -1,19 +1,36 @@
 import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
-export  async function Cart({
-  params
-} : {
-  params: { user: string }
-}): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+export async function GET() {
+  try {
+    // Create the test_table
+    await sql`
+      CREATE TABLE IF NOT EXISTS test_table (
+        id SERIAL PRIMARY KEY,
+        text_column TEXT
+      )
+    `;
 
-  return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
-  );
+    console.log("Table created successfully");
+
+    // Get the list of all tables
+    const result = await sql`
+      SELECT table_name 
+      FROM information_schema.tables 
+      WHERE table_schema = 'public'
+    `;
+
+    const tableNames = result.rows.map(row => row.table_name);
+
+    return NextResponse.json({
+      message: "Tables created successfully",
+      tables: tableNames
+    }, { status: 200 });
+
+  } catch (error) {
+    console.error("Error creating table:", error);
+    return NextResponse.json({
+      message: "Error creating table"
+    }, { status: 500 });
+  }
 }
