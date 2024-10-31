083aab3fec023e0a3ea8d4c03f1a856c88930667
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-10-31 15:41:40 +0100

    Update files

--- src/components/metis-popup.tsx
+++ src/components/metis-popup.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { X, ArrowLeft } from 'lucide-react'
 import './MetisPopup.css'
 import metiNFT from '../../public/MetilNFT.png'
+import { Button } from './ui/button'
 
 const MetisPopup = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -29,13 +30,11 @@ const MetisPopup = () => {
         
         {showMintInterface ? (
           <div className="relative h-full bg-[#1a1b1f]">
-            <button 
+            <Button 
               onClick={handleBack}
-              className="absolute top-4 left-4 z-20 flex items-center gap-2 text-white-300 hover:text-white transition-colors"
             >
               <ArrowLeft size={20} />
-              Back
-            </button>
+            </Button>
             <div className="h-full pt-14">
               <iframe
                 src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0x4de4F5eCad3d6B145450467d6B592b58F6aB7F6f&chain=%7B%22name%22%3A%22Metis+Sepolia+Testnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2F59902.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22tMetis%22%2C%22symbol%22%3A%22tMETIS%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22metis-sepolia%22%2C%22chainId%22%3A59902%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22metis-sepolia-testnet%22%7D&clientId=e4d51769fcc92b76042b7b13f041e01e&theme=dark&primaryColor=purple"
