359c1da3d05d9b5234daf12b3fd2a8a1e2689498
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-10-31 12:47:56 +0100

    Update files

---
+++ src/lib/initializedContract.ts
@@ -0,0 +1,15 @@
+import { createThirdwebClient, getContract } from "thirdweb";
+import { defineChain } from "thirdweb/chains";
+
+// create the client with your clientId, or secretKey if in a server environment
+const client = createThirdwebClient({
+  clientId: "e4d51769fcc92b76042b7b13f041e01e"
+ });
+
+// connect to your contract
+export const contract = getContract({
+  client,
+  chain: defineChain(59902),
+  address: "0x4de4F5eCad3d6B145450467d6B592b58F6aB7F6f"
+});
+
