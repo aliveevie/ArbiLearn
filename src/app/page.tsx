9af216db82111c90a0ed4f869f21e3302d5e2339
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-12-17 22:20:27 +0100

    Update files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import { Hero } from "@/components/hero"
 import { HeaderComponent } from "@/components/header"
 import { WelcomeSection } from "./sections/welcome"
 import { VantaGlobe } from './sections/vanta'
+import HeroSectionTwo from "./sections/section2"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
        <WelcomeSection />
+       <HeroSectionTwo />
     </main>
   )
 }
