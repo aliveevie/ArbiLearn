9a362c49a387114faefc670fedf46bde57754198
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2025-03-18 12:17:36 +0100

    fix the page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,6 @@ import HeroSection3 from "./sections/sectionthree"
 import MobilePopup from "./sections/popupBox"
 import SectionFour from "./sections/sectionFour"
 import SectionFive from "./sections/sectionFive"
-import { recreateVerificationsTable } from "@/lib/db-tables"
-
-export async function initializeDatabase() {
-  try {
-    await recreateVerificationsTable();
-    return { success: true };
-  } catch (error) {
-    console.error("Error initializing database:", error);
-    return { success: false, error };
-  }
-}
-
 export default function Home() {
 
   return (
